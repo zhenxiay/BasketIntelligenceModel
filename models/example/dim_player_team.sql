3186a94e65db37d3b2319949d8ab74d82cfe1d88
unknown <yu.zhenxiao.yz@gmail.com>
2024-12-27 18:42:10 +0100

    Add addtional tables and views to model

---
+++ models/example/dim_player_team.sql
@@ -0,0 +1,7 @@
+
+{{ config(materialized='table') }}
+
+SELECT distinct a.Player, 
+                a.Team, 
+                concat(a.Player,'_',a.Team) as player_team
+FROM `BasketIntelligence`.per_game_stats a
