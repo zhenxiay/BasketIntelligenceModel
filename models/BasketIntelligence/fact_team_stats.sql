a2834eac07a2a54095e7e2ee5f7cb710335ea9f4
zhenxiay <yu.zhenxiao.yu@gmail.com>
2025-01-06 17:13:07 +0100

    Update fact_team_stats.sql

--- models/BasketIntelligence/fact_team_stats.sql
+++ models/BasketIntelligence/fact_team_stats.sql
@@ -1,7 +1,21 @@
 {{ config(materialized='view') }}
 
 SELECT a.*,
-       b.*
+       2P-FGA,
+       0-3-FGA,
+       3-10-FGA,
+       10-16-FGA,
+       16-3P-FGA,
+       3P-FGA,
+       2P-pct,
+       0-3-pct,
+       3-10-pct,
+       10-16-pct,
+       16-3P-pct,
+       3P-pct,
+       3PA-corner,
+       3P-corner
+
 FROM `BasketIntelligence`.team_adv_stats a
 JOIN `BasketIntelligence`.team_shooting b 
 ON a.Team = b.Team
