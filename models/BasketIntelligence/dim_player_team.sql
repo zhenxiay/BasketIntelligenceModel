0122994333c9f6cc3abe5819003e8c35aa476eef
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-01-06 16:49:22 +0100

    Update dim_player_team.sql

--- models/BasketIntelligence/dim_player_team.sql
+++ models/BasketIntelligence/dim_player_team.sql
@@ -2,5 +2,8 @@
 
 SELECT distinct a.Player, 
                 a.Team, 
-                concat(a.Player,'_',a.Team) as player_team
+                concat(a.Player,'_',a.Team) as player_team,
+								b.Conference
 FROM `BasketIntelligence`.per_game_stats a
+JOIN 'BasketIntelligence'.team_conf b
+ON a. Team = b.Team
